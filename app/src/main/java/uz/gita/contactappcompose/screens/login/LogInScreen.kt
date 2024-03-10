1561e78a00717017d863dfae60de7c1eac921902
Tohir Mirxomitov <mirxtohir@gmail.com>
2024-03-10 11:04:55 +0500

    commit

--- app/src/main/java/uz/gita/contactappcompose/screens/login/LogInScreen.kt
+++ app/src/main/java/uz/gita/contactappcompose/screens/login/LogInScreen.kt
@@ -1,27 +1,33 @@
 package uz.gita.contactappcompose.screens.login
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
-import uz.gita.contactappcompose.ui.items.ContactItem
-import uz.gita.contactappcompose.ui.theme.BlackColor
+import uz.gita.contactappcompose.R
 
 class LogInScreen : Screen {
     @Composable
@@ -32,7 +38,69 @@ class LogInScreen : Screen {
 
 @Composable
 fun LogInScreeContent() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(2f),
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "",
+        )
 
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = "Login",
+            color = Color.Black,
+            fontSize = 24.sp,
+        )
+
+        var firstName by remember { mutableStateOf("") }
+        OutlinedTextField(
+            modifier = Modifier,
+            value = firstName,
+            onValueChange = { firstName = it })
+        Spacer(modifier = Modifier.height(16.dp))
+        var lastName by remember { mutableStateOf("") }
+        OutlinedTextField(
+            modifier = Modifier,
+            value = lastName,
+            onValueChange = { lastName = it })
+        Spacer(modifier = Modifier.height(16.dp))
+        var password by remember { mutableStateOf("") }
+        var showPassword by remember { mutableStateOf(false) }
+
+        OutlinedTextField(
+            modifier = Modifier,
+            value = password,
+            onValueChange = { password = it },
+            visualTransformation = if (showPassword) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = { showPassword = !showPassword },
+                    content = {
+                        Icon(
+                            modifier = Modifier.clickable {
+                                showPassword = !showPassword
+                            },
+                            painter = painterResource(
+                                id = if (showPassword) R.drawable.eye
+                                else R.drawable.hidden
+                            ),
+                            contentDescription = "icon"
+                        )
+                    }
+                )
+            }
+        )
+        
+        Button(onClick = {}) {
+            
+        }
+    }
 }
 
 
---
+++ app/src/main/java/uz/gita/contactappcompose/ui/components/ButtonComponent.kt
@@ -0,0 +1,73 @@
+package uz.gita.contactappcompose.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import uz.gita.contactappcompose.ui.theme.ContactAppComposeTheme
+
+@Composable
+fun ButtonComponent(
+    text: String,
+    onClicked: (() -> Unit),
+    modifier: Modifier,
+    isLoading: Boolean = false,
+    enabled: Boolean = true,
+    enabledColor: Color = Color.Blue,
+    disabledColor: Color = Color.Blue.copy(alpha = 0.5f)
+) {
+
+    Button(
+        onClick = onClicked,
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = enabledColor,
+            disabledContainerColor = disabledColor
+        ),
+        content = {
+            if (isLoading) {
+                CircularProgressIndicator(
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(12.dp)
+                        .size(24.dp),
+                    strokeWidth = 2.dp
+                )
+            } else {
+                Row {
+                    Text(text)
+                }
+            }
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        shape = RoundedCornerShape(12.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ButtonComponentPreview() {
+    ContactAppComposeTheme {
+        ButtonComponent(
+            text = "Sample",
+            onClicked = { },
+            isLoading = false,
+            enabled = true,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+    }
+}
+
